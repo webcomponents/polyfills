15e5b14f91358eaeb7d7e2a78e8469327a620721
Daniel Freedman <dfreedm@google.com>
2016-09-30 14:45:29 -0700

    [ci skip]Fix license header for uglified output

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,19 @@ let buffer = require('vinyl-buffer');
 let closureCompiler = compilerPackage.gulp();
 let uglify = require('gulp-uglify');
 let buble = require('rollup-plugin-buble')
+let header = require('gulp-header');
+
+let licenseHeader =
+`/*
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+`;
 
 gulp.task('closure', function() {
   return gulp.src(['./src/*.js'], {base: './'})
@@ -41,32 +54,33 @@ gulp.task('closure', function() {
     .pipe(gulp.dest('./'))
 });
 
-gulp.task(`default`, () => {
+gulp.task('default', () => {
   return rollup({
-    entry: `./src/env.js`,
+    entry: './src/env.js',
     format: 'iife',
     plugins: [buble()],
-    moduleName: `shadydom`,
+    moduleName: 'shadydom',
     sourceMap: true
   })
-  .pipe(source(`env.js`, './src/'))
+  .pipe(source('env.js', './src/'))
   .pipe(buffer())
   .pipe(rename('shadydom.min.js'))
   .pipe(sourcemaps.init({loadMaps: true}))
   .pipe(uglify())
+  .pipe(header(licenseHeader))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('./'))
 });
 
-gulp.task(`debug`, () => {
+gulp.task('debug', () => {
   return rollup({
-    entry: `./src/env.js`,
+    entry: './src/env.js',
     format: 'iife',
     plugins: [buble()],
-    moduleName: `shadydom`,
+    moduleName: 'shadydom',
     sourceMap: true
   })
-  .pipe(source(`env.js`, './src/'))
+  .pipe(source('env.js', './src/'))
   .pipe(buffer())
   .pipe(rename('shadydom.min.js'))
   .pipe(sourcemaps.init({loadMaps: true}))
