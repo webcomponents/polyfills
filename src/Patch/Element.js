3ae1f1e649958a42911ff8ad6e5c0a3987d31b6e
Russell Bicknell <bicknellr@google.com>
2017-02-07 12:00:42 -0800

    Fix `innerHTML` for IE11's tree disassembly behavior.

--- src/Patch/Element.js
+++ src/Patch/Element.js
@@ -33,13 +33,32 @@ export default function(internals) {
       configurable: true,
       get: baseDescriptor.get,
       set: /** @this {Element} */ function(htmlString) {
-        const removedNodes = Array.prototype.slice.apply(this.childNodes);
+        const isConnected = Utilities.isConnected(this);
+
+        // NOTE: In IE11, when using the native `innerHTML` setter, all nodes
+        // that were previously descendants of the context element have all of
+        // their children removed as part of the set - the entire subtree is
+        // 'disassembled'. This work around walks the subtree *before* using the
+        // native setter.
+        /** @type {!Array<!Element>|undefined} */
+        let removedElements = undefined;
+        if (isConnected) {
+          removedElements = [];
+          Utilities.walkDeepDescendantElements(this, element => {
+            if (element !== this) {
+              removedElements.push(element);
+            }
+          });
+        }
 
         baseDescriptor.set.call(this, htmlString);
 
-        if (Utilities.isConnected(this)) {
-          for (let i = 0; i < removedNodes.length; i++) {
-            internals.disconnectTree(removedNodes[i]);
+        if (removedElements) {
+          for (let i = 0; i < removedElements.length; i++) {
+            const element = removedElements[i];
+            if (element.__CE_state === CEState.custom) {
+              internals.disconnectedCallback(element);
+            }
           }
         }
 
