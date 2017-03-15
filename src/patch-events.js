01ea4271efadc748ecd5726922fbe1aad75b72d9
Darryl Yeo <darryl@darryl-yeo.com>
2017-03-15 13:54:05 -0700

    Preserve scope of event callbacks
    Fixes #66.

--- src/patch-events.js
+++ src/patch-events.js
@@ -299,7 +299,7 @@ export function addEventListener(type, fn, optionsOrCapture) {
           return;
         }
       }
-      return fn(e);
+      return fn.call(this, e);
     }
   };
   // Store the wrapper information.
