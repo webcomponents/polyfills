a3dd08e9f4442f37b76c1903605532d83b3ef6e5
Daniel Freedman <dfreedm@google.com>
2017-05-09 17:33:43 -0700

    clean up settings parsing for disabling css custom property support

--- src/style-settings.js
+++ src/style-settings.js
@@ -11,28 +11,28 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 'use strict';
 
 export let nativeShadow = !(window['ShadyDOM'] && window['ShadyDOM']['inUse']);
-// chrome 49 has semi-working css vars, check if box-shadow works
-// safari 9.1 has a recalc bug: https://bugs.webkit.org/show_bug.cgi?id=155782
-export let nativeCssVariables = Boolean(!navigator.userAgent.match('AppleWebKit/601') &&
-window.CSS && CSS.supports && CSS.supports('box-shadow', '0 0 0 var(--foo)'));
+export let nativeCssVariables;
 
 /**
  * @param {(ShadyCSSOptions | ShadyCSSInterface)=} settings
  */
-function parseSettings(settings) {
+function calcCssVariables(settings) {
   if (settings && settings['shimcssproperties']) {
     nativeCssVariables = false;
+  } else {
+    // chrome 49 has semi-working css vars, check if box-shadow works
+    // safari 9.1 has a recalc bug: https://bugs.webkit.org/show_bug.cgi?id=155782
+    nativeCssVariables = Boolean(!navigator.userAgent.match('AppleWebKit/601') &&
+      window.CSS && CSS.supports && CSS.supports('box-shadow', '0 0 0 var(--foo)'));
   }
 }
 
-if (window.ShadyCSS) {
-  if (window.ShadyCSS.nativeCss !== undefined) {
-    nativeCssVariables = window.ShadyCSS.nativeCss;
-  } else {
-    parseSettings(window.ShadyCSS);
-    // reset window.ShadyCSS to set up the API
-    window.ShadyCSS = null;
-  }
-} else if (window['WebComponents']) {
-  parseSettings(window['WebComponents']['flags']);
+if (window.ShadyCSS && window.ShadyCSS.nativeCss !== undefined) {
+  nativeCssVariables = window.ShadyCSS.nativeCss;
+} else if (window.ShadyCSS) {
+  calcCssVariables(window.ShadyCSS);
+  // reset window variable to let ShadyCSS API take its place
+  window.ShadyCSS = undefined;
+} else {
+  calcCssVariables(window['WebComponents'] && window['WebComponents']['flags']);
 }
