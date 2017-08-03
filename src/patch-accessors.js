d624c0b8c70dfa65df556c8c83060fd33256590f
Daniel Freedman <dfreedm@google.com>
2017-08-03 12:30:54 -0700

    Clear document fragments if setting a blank string
    Adapted from @vlukashov's PR #122
    Fixes #122

--- src/patch-accessors.js
+++ src/patch-accessors.js
@@ -253,7 +253,10 @@ let InsideAccessors = {
         case Node.ELEMENT_NODE:
         case Node.DOCUMENT_FRAGMENT_NODE:
           clearNode(this);
-          this.appendChild(document.createTextNode(text));
+          // Document fragments must have no childnodes if setting a blank string
+          if (text.length > 0 || this.nodeType === Node.ELEMENT_NODE) {
+            this.appendChild(document.createTextNode(text));
+          }
           break;
         default:
           // TODO(sorvell): can't do this if patch nodeValue.
