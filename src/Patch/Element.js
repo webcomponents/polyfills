2cafdc03293c59d88d3b2a69963a9cebfa7390b9
Russell Bicknell <bicknellr@google.com>
2017-09-18 14:02:53 -0700

    Wrap `Element#innerHTML` tests; fix `ownerDocument` access in `Element#innerHTML` patch.

--- src/Patch/Element.js
+++ src/Patch/Element.js
@@ -75,7 +75,7 @@ export default function(internals) {
 
         // Only create custom elements if this element's owner document is
         // associated with the registry.
-        if (!this.ownerDocument.__CE_hasRegistry) {
+        if (!NodeProxy.ownerDocument(this).__CE_hasRegistry) {
           internals.patchTree(this);
         } else {
           internals.patchAndUpgradeTree(this);
