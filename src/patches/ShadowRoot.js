08fbe5dfcb2ecafa6e9c981325bdfe6f246a1363
Daniel Freedman <dfreedm@google.com>
2019-03-04 11:14:52 -0800

    Fix dispatching event from ShadowRoot
    Fixes #321

--- src/patches/ShadowRoot.js
+++ src/patches/ShadowRoot.js
@@ -42,6 +42,17 @@ export const ShadowRootPatches = utils.getOwnPropertyDescriptors({
     }
     optionsOrCapture.__shadyTarget = this;
     this.host[utils.SHADY_PREFIX + 'removeEventListener'](type, fn, optionsOrCapture);
+  },
+
+  /**
+   * @this {ShadowRoot}
+   * @param {!Event} event
+   * @return {boolean} cancelled
+   */
+  dispatchEvent(event) {
+    // define target on event to have it distinguishable from the host
+    Object.defineProperty(event, 'target', {value: this, configurable: true, writable: true});
+    return this.host[utils.SHADY_PREFIX + 'dispatchEvent'](event);
   }
 
 });
