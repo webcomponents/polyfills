394e5596763af48dabf7bcaf8c00ef2cca224a8b
Tim van der Lippe <tvanderlippe@google.com>
2019-04-18 12:21:25 +0100

    Add @webcomponents/shadycss

---
+++ packages/shadycss/externs/shadycss-externs.js
@@ -0,0 +1,30 @@
+/** @externs */
+
+/** @typedef {{
+ * styleElement: function(!HTMLElement),
+ * styleSubtree: function(!HTMLElement, Object<string, string>=),
+ * prepareTemplate: function(!HTMLTemplateElement, string, string=),
+ * prepareTemplateStyles: function(!HTMLTemplateElement, string, string=),
+ * prepareTemplateDom: function(!HTMLTemplateElement, string),
+ * styleDocument: function(Object<string, string>=),
+ * flushCustomStyles: function(),
+ * getComputedStyleValue: function(!Element, string): string,
+ * ScopingShim: (Object|undefined),
+ * ApplyShim: (Object|undefined),
+ * CustomStyleInterface: (Object|undefined),
+ * nativeCss: boolean,
+ * nativeShadow: boolean,
+ * }}
+ */
+let ShadyCSSInterface; //eslint-disable-line no-unused-vars
+
+/**
+ * @typedef {{
+ * shimcssproperties: (boolean | undefined),
+ * shimshadow: (boolean | undefined)
+ * }}
+ */
+let ShadyCSSOptions; //eslint-disable-line no-unused-vars
+
+/** @type {(ShadyCSSInterface | ShadyCSSOptions | undefined)} */
+window.ShadyCSS;
