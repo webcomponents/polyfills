0f5b3cc3044964dd1f302436c077bddb59d89b20
Steven Orvell <sorvell@google.com>
2019-11-01 11:56:11 -0700

    Address feedback review

--- packages/shadydom/src/utils.js
+++ packages/shadydom/src/utils.js
@@ -35,7 +35,7 @@ export const isTrackingLogicalChildNodes = (node) => {
 }
 
 export const isShadyRoot = (obj) => {
-  return Boolean(obj instanceof DocumentFragment && obj._localName === 'ShadyRoot');
+  return obj instanceof ShadowRoot;
 }
 
 export const hasShadowRootWithSlot = (node) => {
