94871d52ef0e76817d15d09b67af29ab827727f2
Russell Bicknell <bicknellr@google.com>
2020-03-02 17:27:36 -0800

    packages/shadycss/tests/ -> packages/tests/shadycss/

---
+++ packages/tests/shadycss/module/custom-style-element.js
@@ -0,0 +1,18 @@
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+/*
+A simple shim to generate a testable module
+*/
+
+// TODO: This reference reaches directly into the shadycss package because this
+// file is not available in the npm package. Is there a better way to reference
+// this file?
+import '../../../shadycss/examples/custom-style-element'
