71a07278204be6ee86a56cbe6b815077e8b32d30
Peter Burns <rictic@google.com>
2020-04-13 13:57:24 -0700

    Convert webcomponentsjs platform to TypeScript

--- packages/webcomponentsjs/ts_src/platform/symbol.ts
+++ packages/webcomponentsjs/ts_src/platform/symbol.ts
@@ -1,15 +1,16 @@
 /**
 @license
 Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 
 // import polyfill for Symbol and Object.getOwnPropertySymbols
-import '../../node_modules/get-own-property-symbols/build/get-own-property-symbols.max.js';
+import 'get-own-property-symbols/build/get-own-property-symbols.max';
 
 // Fix issue in toString patch when compiled into strict mode via closure
 // https://github.com/es-shims/get-own-property-symbols/issues/16
@@ -22,10 +23,10 @@ Object.prototype.toString = function() {
   } else {
     return toString.call(this);
   }
-}
+};
 
 // overwrite Object.keys to filter out symbols
-Object.keys = function(obj) {
+Object.keys = function(obj: object) {
   return Object.getOwnPropertyNames(obj).filter((name) => {
     const prop = Object.getOwnPropertyDescriptor(obj, name);
     return prop && prop.enumerable;
@@ -33,21 +34,17 @@ Object.keys = function(obj) {
 };
 
 // implement iterators for IE 11
-const iterator = window.Symbol.iterator;
-
-if (!String.prototype[iterator] || !String.prototype.codePointAt) {
-  /** @this {String} */
-  String.prototype[iterator] = function*() {
+if (!String.prototype[Symbol.iterator] || !String.prototype.codePointAt) {
+  String.prototype[Symbol.iterator] = function*(this: string) {
     for (let i = 0; i < this.length; i++) {
       yield this[i];
     }
   }
 }
 
-if (!Set.prototype[iterator]) {
-  /** @this {Set} */
-  Set.prototype[iterator] = function*() {
-    const temp = [];
+if (!Set.prototype[Symbol.iterator]) {
+  Set.prototype[Symbol.iterator] = function*<T>(this: Set<T>) {
+    const temp: T[] = [];
     this.forEach((value) => {
       temp.push(value);
     });
@@ -57,14 +54,13 @@ if (!Set.prototype[iterator]) {
   };
 }
 
-if (!Map.prototype[iterator]) {
-  /** @this {Map} */
-  Map.prototype[iterator] = function*() {
-    const entries = [];
+if (!Map.prototype[Symbol.iterator]) {
+  Map.prototype[Symbol.iterator] = function*<K, V>(this: Map<K, V>) {
+    const entries: Array<[K, V]> = [];
     this.forEach((value, key) => {
       entries.push([key, value]);
     });
-    for(let i = 0; i < entries.length; i++) {
+    for (let i = 0; i < entries.length; i++) {
       yield entries[i];
     }
   };
