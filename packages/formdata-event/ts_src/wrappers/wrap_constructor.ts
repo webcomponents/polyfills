cd1d92330d96449bdc885da7a8f619cae93984c9
Russell Bicknell <bicknellr@google.com>
2020-07-10 12:41:03 -0700

    Add license header.

--- packages/formdata-event/ts_src/wrappers/wrap_constructor.ts
+++ packages/formdata-event/ts_src/wrappers/wrap_constructor.ts
@@ -1,3 +1,14 @@
+/**
+ * @license
+ * Copyright (c) 2020 The Polymer Project Authors. All rights reserved. This
+ * code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt The complete set of authors may be
+ * found at http://polymer.github.io/AUTHORS.txt The complete set of
+ * contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt Code
+ * distributed by Google as part of the polymer project is also subject to an
+ * additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
 export const wrapConstructor = (Wrapper: any, Constructor: any, prototype: any) => {
   for (const prop of Object.keys(Constructor)) {
     // `Event.prototype` is not writable or configurable in Safari 9. We
