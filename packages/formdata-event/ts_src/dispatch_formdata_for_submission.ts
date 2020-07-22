b82fc129220a81543048c1663ddb11968b8962ed
Russell Bicknell <bicknellr@google.com>
2020-07-22 15:04:57 -0700

    Comments for `dispatchFormdataForSubmission`.

--- packages/formdata-event/ts_src/dispatch_formdata_for_submission.ts
+++ packages/formdata-event/ts_src/dispatch_formdata_for_submission.ts
@@ -11,22 +11,51 @@
 
 import {getEntries} from './wrappers/form_data.js';
 
+/**
+ * Dispatches a 'formdata' event to `form` and modifies the form to reflect any
+ * changes made to the `FormData` instance that was sent along with the event.
+ *
+ * In browsers without native support for the 'formdata' event, the only way to
+ * affect the data sent during form submission is to modify the form-associated
+ * elements in the form before it is submitted. The form can be modified up
+ * until the 'submit' event has completed dispatch.
+ */
 export const dispatchFormdataForSubmission = (form: HTMLFormElement) => {
-  // Constructing this FormData with `form` dispatches the 'formdata' event.
+  // Constructing this FormData _synchronously_ dispatches the 'formdata' event
+  // to `form` and, once complete, `formData` can be used to retrieve any
+  // changes to the entry list.
   const formData = new FormData(form);
 
+  /**
+   * Any inputs inserted into the form while modifying it to match entries of
+   * `formData`.
+   */
   const insertedInputs: Array<HTMLInputElement> = [];
+
+  /**
+   * Appends a hidden input to the form with the given name and value.
+   */
   const appendEntry = (name: string, value: string) => {
     const input = document.createElement('input');
     input.type = 'hidden';
     input.name = name;
     input.value = value;
-    form.appendChild(input);
 
+    form.appendChild(input);
     insertedInputs.push(input);
   };
 
+  /**
+   * The initial value of the 'disabled' attribute of any form-associated
+   * element that has its 'disabled' attribute modified to match entries of
+   * `formData`.
+   */
   const disabledInitialValue = new Map<Element, string | null>();
+
+  /**
+   * Disables any form-associated elements that have the given name, including
+   * those inserted by `appendEntry`.
+   */
   const disableExistingEntries = (name: string) => {
     for (let i = 0; i < form.elements.length; i++) {
       const element = form.elements[i];
@@ -40,6 +69,9 @@ export const dispatchFormdataForSubmission = (form: HTMLFormElement) => {
     }
   };
 
+  /**
+   * Finds the first non-disabled form-associated element with a given name.
+   */
   const findFirstEnabledElement = (name: string): Element | undefined => {
     for (let i = 0; i < form.elements.length; i++) {
       const element = form.elements[i];
@@ -50,6 +82,7 @@ export const dispatchFormdataForSubmission = (form: HTMLFormElement) => {
     return undefined;
   };
 
+  // Update the form to match the entries in `formData`.
   for (const entry of getEntries(formData)!) {
     switch (entry.operation) {
       case 'append': {
@@ -62,21 +95,29 @@ export const dispatchFormdataForSubmission = (form: HTMLFormElement) => {
 
       case 'set': {
         const {name, value} = entry;
-
         const first = findFirstEnabledElement(name);
+
+        // If there are no entries (i.e. enabled, form-associated elements) with
+        // the given name, then setting an entry is the same as appending it.
         if (first === undefined) {
           appendEntry(name, value);
-          break;
+        // If there are entries with the given name, then setting an entry
+        // should overwrite the _first_ entry in the list with that name and
+        // delete all other entries.
+        } else {
+          disableExistingEntries(name);
+
+          // Insert the input representing the new entry into the form
+          // immediately before the first enabled element with that name (before
+          // it was disabled above) so that it appears to replace it in the
+          // entry list.
+          const input = document.createElement('input');
+          input.type = 'hidden';
+          input.name = name;
+          input.value = value;
+          first.parentNode!.insertBefore(input, first);
+          insertedInputs.push(input);
         }
-
-        disableExistingEntries(name);
-
-        const input = document.createElement('input');
-        input.type = 'hidden';
-        input.name = name;
-        input.value = value;
-        first.parentNode!.insertBefore(input, first);
-        insertedInputs.push(input);
       } break;
 
       default:
@@ -84,11 +125,19 @@ export const dispatchFormdataForSubmission = (form: HTMLFormElement) => {
     }
   }
 
+  // Undo any modifications made to the form.
+  //
+  // This is delayed until at least the next task because the browser will read
+  // data out of the form when the current task completes. Typically, the form
+  // will already have been submitted and the page will be destroyed before this
+  // runs.
   setTimeout(() => {
+    // Remove any inserted inputs.
     for (const input of insertedInputs) {
       input.parentNode?.removeChild(input);
     }
 
+    // Restore the 'disabled' attribute state of any modified form elements.
     for (const [element, value] of disabledInitialValue) {
       if (element.getAttribute('disabled') !== value) {
         if (value === null) {
