a884e865b37339c295906e6da37b1e25306e6fc4
Russell Bicknell <bicknellr@google.com>
2022-03-30 16:51:53 -0700

    Add trusted types headers to pages specified with the `--enforce-trusted-types` option.

--- wct-trusted-types-plugin/main.js
+++ wct-trusted-types-plugin/main.js
@@ -1,3 +1,32 @@
-module.exports = (context, pluginOptions, plugin) => {
-  console.log(context, pluginOptions, plugin);
+const express = require('express');
+
+module.exports = (context, pluginOptions = {}, plugin) => {
+  const pathToPolicies = new Map();
+  for (const rule of pluginOptions['enforce-trusted-types'] ?? []) {
+    const [path, policies] = rule.split(';');
+    pathToPolicies.set(path, policies?.split(',') ?? []);
+  }
+
+  context.hook('define:webserver', (app, assign, options, done) => {
+    const newApp = express();
+
+    newApp.use((req, res, next) => {
+      const policies = pathToPolicies.get(req.path);
+      if (policies !== undefined) {
+        res.append(
+          'Content-Security-Policy',
+          `require-trusted-types-for 'script'`
+        );
+        res.append(
+          'Content-Security-Policy',
+          `trusted-types ${policies.join('')}`
+        );
+      }
+      next();
+    });
+
+    newApp.use(app);
+    assign(newApp);
+    done();
+  });
 };
