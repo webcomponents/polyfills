eac61a454353641a0ae14aa6b94309fec923ca8b
Russell Bicknell <bicknellr@google.com>
2022-03-31 13:32:30 -0700

    Use a query parameter to set the trusted types policies, rather than a command line option.

--- wct-trusted-types-plugin/main.js
+++ wct-trusted-types-plugin/main.js
@@ -1,26 +1,17 @@
 const express = require('express');
 
 module.exports = (context, pluginOptions = {}, _plugin) => {
-  const pathToPolicies = new Map();
-  for (const rule of pluginOptions['enforce-trusted-types'] ?? []) {
-    const [path, policies] = rule.split(';');
-    pathToPolicies.set(path, policies?.split(',') ?? []);
-  }
-
   context.hook('define:webserver', (app, assign, options, done) => {
     const newApp = express();
 
     newApp.use((req, res, next) => {
-      const policies = pathToPolicies.get(req.path);
+      const policies = req.query['enforce-trusted-types'];
       if (policies !== undefined) {
         res.append(
           'Content-Security-Policy',
           `require-trusted-types-for 'script'`
         );
-        res.append(
-          'Content-Security-Policy',
-          `trusted-types ${policies.join(' ')}`
-        );
+        res.append('Content-Security-Policy', `trusted-types ${policies}`);
       }
       next();
     });
