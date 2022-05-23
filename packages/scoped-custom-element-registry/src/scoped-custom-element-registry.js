e2acf58a0e9de18effd64238ea0d8c11d3a80630
Danny Moerkerke <danny@dannymoerkerke.nl>
2022-05-23 16:00:38 +0200

    - added `toggleAttribute` to patched methods to make sure `attributeChangedCallback` is executed when it is called

--- packages/scoped-custom-element-registry/src/scoped-custom-element-registry.js
+++ packages/scoped-custom-element-registry/src/scoped-custom-element-registry.js
@@ -346,6 +346,20 @@ if (!ShadowRoot.prototype.createElement) {
         }
       };
     }
+    const toggleAttribute = elementClass.prototype.toggleAttribute;
+    if (toggleAttribute) {
+      elementClass.prototype.toggleAttribute = function (n) {
+        const name = n.toLowerCase();
+        if (observedAttributes.has(name)) {
+          const old = this.hasAttribute(name);
+          toggleAttribute.call(this, name);
+          const hasAttribute = this.hasAttribute(name);
+          attributeChangedCallback.call(this, name, old, hasAttribute);
+        } else {
+          toggleAttribute.call(this, name);
+        }
+      };
+    }
   };
 
   // Helper to patch CE class hierarchy changing those CE classes created before applying the polyfill
