c03e32c8a51984ac146cfa881521f897ef90f09e
Russell Bicknell <bicknellr@google.com>
2022-06-28 15:35:19 -0700

    Add a note about why Chai must be bundled despite having a module entrypoint.

--- packages/tests/chai.js
+++ packages/tests/chai.js
@@ -1,6 +1,12 @@
 /**
  * This is the entrypoint for the Chai bundle used as part of the test
  * environment. Chai is only distributed as Common JS, so it must be bundled.
+ *
+ * Despite the `.mjs` extension, the module referenced here[^1] imports another
+ * file[^2] that itself attempts to import the entire library using `require()`.
+ *
+ * [^1]: https://github.com/chaijs/chai/blob/v4.3.6/index.mjs
+ * [^2]: https://github.com/chaijs/chai/blob/v4.3.6/index.js
  */
 
 export * from 'chai/index.mjs';
