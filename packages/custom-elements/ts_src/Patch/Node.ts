d39ab05981630974378155bb521cdda74b80466a
Aierie <39579264+Aierie@users.noreply.github.com>
2022-07-29 20:49:02 +0800

    improve fix using nullish coalescing

--- packages/custom-elements/ts_src/Patch/Node.ts
+++ packages/custom-elements/ts_src/Patch/Node.ts
@@ -92,14 +92,10 @@ export default function (internals: CustomElementInternals) {
 
   Node.prototype.cloneNode = function (this: Node, deep) {
     const clone = Native.Node_cloneNode.call(this, !!deep);
-    // document.ownerDocument is null
-    const registry = this.ownerDocument
-      ? this.ownerDocument!.__CE_registry
-      : (this as Document).__CE_registry;
 
     // Only create custom elements if this element's owner document is
     // associated with the registry.
-    if (!registry) {
+    if (!(this.ownerDocument ?? this).__CE_registry) {
       internals.patchTree(clone);
     } else {
       internals.patchAndUpgradeTree(clone);
